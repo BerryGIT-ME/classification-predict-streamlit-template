fb8f20a0129d20e976af17939805f22aa77e6151
Ikechukwu Okerenwogba <ike.okerenwogba@gmail.com>
2022-04-07 13:06:13 +0100

    removed demo added train.csv

--- base_app.py
+++ base_app.py
@@ -29,7 +29,8 @@ import pandas as pd
 from utils import preprocess, predict, get_random_sample
 
 # Load your raw data
-raw = pd.read_csv("resources/test.csv")
+raw_test = pd.read_csv("resources/test.csv")
+raw_train = pd.read_csv("resources/train.csv")
 
 # The main function where we will build the actual app
 def main():
@@ -38,30 +39,44 @@ def main():
 	text = "text"
 	# Creates a main title and subheader on your page -
 	# these are static across all pages
-	st.title("Data Blaze Inc Tweet Classifer Demo")
+	st.title("Data Blaze Inc Tweet Classifer")
 	st.subheader("Climate change tweet classification")
 
 	# Creating sidebar with selection box -
 	# you can create multiple pages this way
-	options = ["Prediction", "Information"]
+	options = ["Prediction", "Information", 'Explore the data']
 	selection = st.sidebar.selectbox("Choose Option", options)
 	
 	# Building out the "Information" page
 	if selection == "Information":
+		with open('./resources/info.md', 'r') as file:
+			markdown_text = file.read()
+
 		st.info("General Information")
 		# You can read a markdown file from supporting resources folder
-		st.markdown("Some information here")
+		st.markdown(markdown_text)
 
 		st.subheader("Raw Twitter data and label")
 		if st.checkbox('Show raw data'): # data is hidden if box is unchecked
-			st.write(raw[['message']]) # will write the df to the page
-	
+			st.write(raw_train[['message', 'sentiment']]) # will write the df to the page
+
+	if selection == "Explore the data":
+		with open('./resources/info.md', 'r') as file:
+			markdown_text = file.read()
+
+		st.info("General Information")
+		# You can read a markdown file from supporting resources folder
+		st.markdown(markdown_text)
+
+		st.subheader("Raw Twitter data and label")
+		if st.checkbox('Show raw data'): # data is hidden if box is unchecked
+			st.write(raw_test[['message']]) # will write the df to the page
 	# Building out the predication page
 	if selection == "Prediction":
 		st.info("Prediction with ML Models")
 		# Creating a text box for user input
 		if st.button("Get random tweet"):
-			text = get_random_sample(raw)
+			text = get_random_sample(raw_test)
 			print()
 		st.text_area("Random tweet sample", text)
 
